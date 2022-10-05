2be7a6dc403c1508a636b7f7af93559a26b0ba32
MariaBut <112708831+MariaBut@users.noreply.github.com>
2022-10-05 14:53:05 +0300

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,8 @@
+public class StringCompare {
+    public static void main(String[] args) {}
+    public static boolean compareStr(String Str1, String Str2) {
+        init len1 = Str1.length();
+        init len2 = Str2.length();
+        return len1 > len2;
+    }
+}
