07fa885420aeb515fd11a5da09bf1cb082881e6a
Mehdi83 <mehdy1982_me@yahoo.com>
2022-10-05 22:47:41 +0300

    Modified

--- StringCompare.java
+++ StringCompare.java
@@ -1,8 +1,17 @@
 public class StringCompare {
-    public static void main(String[] args) {}
-    public static boolean compareStr(String Str1, String Str2) {
+    public static void main(String[] args) {
+        StringCompare.compareStr()
+    }
+    public static void compareStr(String Str1, String Str2) {
         init len1 = Str1.length();
         init len2 = Str2.length();
-        return len1 > len2;
+
+        if (len1 > len2) {
+            return true;
+        }
+
+        else if (len1 = len2) {
+           System.out.println("Equal strings lenght");
+        }
     }
 }
